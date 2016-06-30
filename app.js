d3a6603222d3789ac4e0c6fc2e6fa85c660e7295
Dustin Long <dlong@Dustins-MacBook.local>
2016-06-30 11:45:37 -0400

    Express basic server

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+const PORT = 8080;
+
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/html/form.html');
+});
+
+app.get('/process_get', function (request, response) {
+    var total = 0;
+    // TODO: Add code here.
+    response.end(total);
+});
+
+app.listen(PORT, function () {
+  console.log('Example app listening on port ' + PORT);
+});
